45697514ed3b1ce45b3e2f0f3764727d3fb6d57f
MindChirp <frikk44@gmail.com>
2021-12-15 04:19:43 +0100

    Update statistics.js

--- modules/homepage/statistics.js
+++ modules/homepage/statistics.js
@@ -1,4 +1,8 @@
+const path = require("path");
+const fs = require("fs-extra");
+
 function drawSessionGraph() {
+    if(!userConfig.betaTester) return;
     var canv = document.getElementById("stat-container").querySelector("#statistic");
     var ctx = canv.getContext("2d");
     ctx.lineWidth = 20;
@@ -18,10 +22,70 @@ function drawSessionGraph() {
     }
 
 
-    bzCurve({points:lines, f:0.5, t:0.5, ctx: ctx});
-    plotGraphPoints({points:lines, ctx: ctx, size: 0, color: "blue"});
+    //plotGraphPoints({points:lines, ctx: ctx, size: 20, color: "blue"});
     //drawGraphMeta();
+    fetchSessionData()
+    .then(res=>{
 
+        var multiplierGraphHeight = 0.0138;
+        //Arrange the points according to age (date)
+        //Add the same days into a combined poll
+        var days = [];
+        for(let i = 1; i <= 7; i++) {
+            //Create the date object for the day we are checking
+            console.log(i)
+            var date = new Date();
+            date.setTime(date.getTime()-(7-i)*24*60*60*1000);
+
+            //Get the date we're dealing with
+            for(let m = 0; m < res.length; m++) {
+                var datDate = res[m].date;
+                console.log(date.getDate())
+                if(datDate.getDate() == date.getDate() && datDate.getFullYear() == date.getFullYear() && datDate.getMonth() == date.getMonth()) {
+                    //This is the same day, add to an array
+                    try {
+                        var index = days.findIndex(item => item.x === i*(3000/6)-500);
+                        if(index > -1) {
+                            //We've found the object
+                            days[index].y = days[index].y - (res[m].timePlayed*multiplierGraphHeight);
+                        } else {
+                            days.push({x:i*(3000/6)-500,y:300-(res[m].timePlayed*multiplierGraphHeight)});
+                        }
+                    } catch (error) {
+                        console.error(error);
+                    }
+                } else {
+
+                }
+            }
+
+
+            //Check if there exists an entry for this day
+            try {
+                var index = days.findIndex(item => item.x === i*(3000/6)-500);
+                if(index == -1) {
+                    days.push({x:i*(3000/6)-500,y:300});
+                }
+            } catch (error) {
+                days.push({x:i*(3000/6)-500,y:300});
+            }
+            
+
+
+        }
+        
+
+        //days
+        //Now print to the graph!
+        //plotGraphPoints({points:days, ctx: ctx, size: 20, color: "blue"});
+        //console.log(days)
+        bzCurve({points:days, f:0.3, t:0.2, ctx: ctx});
+
+
+    })
+    .catch(err=>{
+        console.error(err);
+    })
 }
 
 
@@ -35,6 +99,47 @@ function plotGraphPoints({points, ctx, size, color}) {
         ctx.closePath();
     }
 
+}
+
+
+function fetchSessionData() {
+    return new Promise((resolve, reject)=>{
+
+        var dataPoints = [];
+
+        //Get the session data from last seven days
+        var sesPath = path.join(filesPath, "recordeddata");
+        fs.readdir(sesPath)
+        .then(async (dat)=>{
+            
+            //Read through each file, get the date
+            for(let i = 0; i < dat.length; i++) {
+                try {
+                    var file = JSON.parse(await fs.readFile(path.join(sesPath, dat[i]), "utf8"));
+                    //Enter the file date to a date object, and check if it should be
+                    //Displayed in the graph
+                    var date = new Date(file.date).getTime()*7/*days*/*24/*hours*/*60/*minutes*/*60/*seconds*/*1000/*milliseconds*/;
+                    var now = new Date().getTime();
+
+                    if(date >= now) {
+                        //This is less than or equals to seven days ago.
+                        dataPoints.push({date: new Date(file.date), timePlayed: file.timePlayed})
+                    } else {
+                        //Discard
+                    }
+                } catch (error) {
+                    console.log(error);
+                }      
+            }
+
+            resolve(dataPoints);
+            
+        })
+        .catch((err)=>{
+            console.log(err);
+            reject(err);
+        })
+    })
 }
 
 
